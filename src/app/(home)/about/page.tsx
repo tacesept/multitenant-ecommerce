5c83d30443b7f6a4742dbf43d73231a97740e9f3
two8 <155454715+tacesept@users.noreply.github.com>
2025-08-30 15:05:31 +0630

    03: home layout

---
+++ src/app/(home)/about/page.tsx
@@ -0,0 +1,6 @@
+const page = () => {
+  return (
+    <div>about</div>
+  )
+}
+export default page
---
+++ src/app/(home)/pricing/page.tsx
@@ -0,0 +1,6 @@
+const page = () => {
+  return (
+    <div>pricing</div>
+  )
+}
+export default page
